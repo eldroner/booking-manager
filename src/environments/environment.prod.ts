243f5198a5bb7e0317537578a91b8f79b919463e
Jose <amadowebs@gmail.com>
2025-07-19 22:09:56 +0200

    arreglado el problema del servidor, que siempre iba a buscar en localhost

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,8 @@
+
 export const environment = {
+  //voy a poner un comentario a ver si me deja guardar como nuevo.
   production: true,
-  apiUrl: 'https://reservas-api.pixelnova.es/api',
+  apiUrl: 'https://reservas-api.pixelnova.es',
   emailjs: {
     serviceId: 'service_8ye06ka',
     templateId: 'template_lhxsfc9',

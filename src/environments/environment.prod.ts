8d841366ea68fdd718ba5dc46cbf79493763b4b4
Jose <amadowebs@gmail.com>
2025-07-19 12:44:51 +0200

    funcionando bien en servidor con thaccess para rutas directas

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://reservas-api.pixelnova.es/api',
+  emailjs: {
+    serviceId: 'service_8ye06ka',
+    templateId: 'template_lhxsfc9',
+    userId: 'pkQl3ciKlncnQMXk3'
+  }
+};
